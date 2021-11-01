535e14600e9dfba56be74ede916ae87fb9555eed
josephWoodland <joseph@workit-app.com>
2021-11-01 19:17:05 +0000

    Commit with a connection issue with the products.html template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+
+def all_products(request):
+    """View to show al products
+        will handle searchs
+    Args:
+        request (GET): Get request
+    Return:
+        products.html
+    """
+
+    products = Product.object.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
