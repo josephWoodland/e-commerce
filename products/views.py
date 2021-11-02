abf8def96d6bbf6f7b2db1256270ee697429fc9f
josephWoodland <joseph@workit-app.com>
2021-11-02 10:04:36 +0000

    Added product detail page

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
 
 def all_products(request):
-    """View to show al products
+    """View to show all products
         will handle searchs
     Args:
         request (GET): Get request
@@ -18,3 +18,21 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """View to show a product
+    Args:
+        request (GET): Get request
+        product_id (str): Product id
+    Return:
+        product<id>.html
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
