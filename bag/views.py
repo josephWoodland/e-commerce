1ff9dd7ef2ac5e2fb4c946b1aa483ac2dab6e0da
Joseph <joseph@workit-app.com>
2021-11-03 17:32:14 +0000

    Revert "Bag function"
    This reverts commit dae7fe831bd16d0012aee396023b5e84d47f76f8.

--- bag/views.py
+++ bag/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 # Create your views here.
+
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+<<<<<<< HEAD
+
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """Add an item to a shopping bag
 
+    Args:
+        request (HTTP): POST request to the server
+        item_id (str): item id string
+    """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -20,4 +27,7 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print('Session: ', request.session['bag'])
     return redirect(redirect_url)
+=======
+>>>>>>> parent of db96be3 (Add to bag functionlity)
