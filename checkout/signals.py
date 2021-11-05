9e45fbf203e21c5808ea1124c2d5a9eb2d98d8f4
josephWoodland <joseph@workit-app.com>
2021-11-05 23:00:16 +0000

    Added to checkout/signals

---
+++ checkout/signals.py
@@ -0,0 +1,27 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender - OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """Update the order total on save
+
+    Args:
+        sender (str): Line Item
+        instance (str): What is going to be updated
+        created (boolen): instance from the server if item needs updating
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender - OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """Update the order total on save
+
+    Args:
+        sender (str): Line Item
+        instance (str): What is going to be updated
+    """
+    instance.order.update_total()
