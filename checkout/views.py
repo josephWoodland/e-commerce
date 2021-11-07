4501ace3275bd6189bf262be9aaaba5e7cbd2212
Joseph <joseph@workit-app.com>
2021-11-07 00:20:11 +0000

    Added the env variables to the .zshrc file in the os

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import redirect, render, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
+import stripe
 # Create your views here.
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    print('KEY: ', stripe_secret_key)
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(amount=stripe_total,
+                                         currency=settings.STRIPE_CURRENCY)
+
+    print('This is the intent: ', intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
