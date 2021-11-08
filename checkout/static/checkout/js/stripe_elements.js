e9bdb39bba6a01ce2d6c221c40108aa048bebd74
josephWoodland <joseph@workit-app.com>
2021-11-08 11:37:19 +0000

    Added the basic function for payment

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,9 +6,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-const stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
-const client_secret = $('#id_client_secret').text().slice(1,-1);
-const stripe = Stripe(stripe_public_key);
+const stripePublicKey = $('#id_stripe_public_key').text().slice(1,-1);
+const clientSecret = $('#id_client_secret').text().slice(1,-1);
+const stripe = Stripe(stripePublicKey);
 const elements = stripe.elements();
 const style = {
     base: {
@@ -46,4 +46,35 @@ card.addEventListener('change', function(e){
     } else {
         errorDiv.textContent = ``;
     }
+});
+
+// Handle form submit
+
+const form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            const errorDiv = document.getElementById('card-errors');
+            const html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
--- checkout/views.py
+++ checkout/views.py
@@ -14,8 +14,6 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    print('KEY: ', stripe_secret_key)
-
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -28,15 +26,17 @@ def checkout(request):
     intent = stripe.PaymentIntent.create(amount=stripe_total,
                                          currency=settings.STRIPE_CURRENCY)
 
-    print('This is the intent: ', intent)
+    if not stripe_public_key:
+        messages.warning(
+            request, 'Stripe public key is missing, \
+                Did you forget to set it in your enviroment?  ')
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key':
-        'pk_test_51JstxtHYYBkvirHbFUhgM6yLKJ8GqkWfG9PYsvzOa18JsHuaTDATGiKdyUCHNsI5ex1xBNbfEznRm7eXlcb58QbK00zowvYvQt',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
