1fef77ab548f3a1b668184a1351ac3469a1fae85
josephWoodland <joseph@workit-app.com>
2021-11-17 17:49:06 +0000

    Updated the Allauth templates with new headers

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import UserProfile
 # Create your views here.
 
 
@@ -9,7 +10,12 @@ def profile(request):
     Args:
         request (HTTP request): Direct to the users profile page
     """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
