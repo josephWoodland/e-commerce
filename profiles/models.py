c7704ddb213cabc79a67a54e3203f1d9e1eeaba2
josephWoodland <joseph@workit-app.com>
2021-11-17 12:01:08 +0000

    Dependency error has occured with django_coutries

---
+++ profiles/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django_coutries.fields import CountryField
+# Create your models here.
+
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+
+    user = models.OneToOneField(user, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20,
+                                            null=True,
+                                            blank=True)
+    default_full_name = models.CharField(max_length=50, null=True, blank=True)
+    default_email = models.EmailField(max_length=254, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *',
+                                   null=True,
+                                   blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40,
+                                            null=True,
+                                            blank=True)
+    default_street_address1 = models.CharField(max_length=80,
+                                               null=False,
+                                               blank=False)
+    default_street_address2 = models.CharField(max_length=80,
+                                               null=True,
+                                               blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@reciver(post_save, sender=User)
+def create_or_upate_user_profile(sender, instance, created, **kwargs):
+    """Create or update the user Profile
+
+    Args:
+        sender ([type]): [description]
+        instance ([type]): [description]
+        created ([type]): [description]
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Exsiting users: just save the profile
+    instance.userprofile.save()
