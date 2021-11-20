58ea23cfb36f0baee9893dd06db736fc35f7f3c3
josephWoodland <joseph@workit-app.com>
2021-11-20 21:13:47 +0000

    Edit product function for store owners

--- products/views.py
+++ products/views.py
@@ -106,3 +106,37 @@ def add_product(request):
         template,
         context,
     )
+
+
+def edit_product(request, product_id):
+    """
+    Edit a product in the store
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'You have updated {product.name}')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request,
+                f'{product.name} has not been updated, please check the details and try again.'
+            )
+
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(
+        request,
+        template,
+        context,
+    )
