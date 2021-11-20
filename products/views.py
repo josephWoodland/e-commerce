ce3af46fbcd0d82cdd72143ec52403b3f3251638
josephWoodland <joseph@workit-app.com>
2021-11-20 22:13:05 +0000

    Delete product function completed

--- products/views.py
+++ products/views.py
@@ -87,9 +87,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Succesfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request,
@@ -140,3 +140,13 @@ def edit_product(request, product_id):
         template,
         context,
     )
+
+
+def delete_product(request, product_id):
+    """
+    Delete the product from the store
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'Product deleted!!')
+    return redirect(reversed('products'))
